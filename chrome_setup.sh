0e9d12a6ee4230b804561a7fab37730897ca94ee
tangent-theta <tomotomo0412mogi@gmail.com>
2022-02-28 17:21:16 +0900

    the first commit

---
+++ chrome_setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# apt update&upgrade
+sudo apt update -y
+sudo apt upgrade -y
+
+#install google-chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+#install fonts
+sudo apt install task-japanese locales-all fonts-ipafont -y
+sudo localectl set-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
+source /etc/default/locale
+
+#install mozc
+sudo apt install fcitx-mozc -y
+
+#change permission
+sudo chmod a+w /etc/systemd/user/cros-garcon.service.d/cros-garcon-override.conf
+
+#set environment variable
+sudo echo 'Environment="GTK_IM_MODULE=fcitx"' > /etc/systemd/user/cros-garcon.service.d/cros-garcon-override.conf
+sudo echo 'Environment="QT_IM_MODULE=fcitx"' > /etc/systemd/user/cros-garcon.service.d/cros-garcon-override.conf
+sudo echo 'Environment="XMODIFIERS=@im=fcitx"' > /etc/systemd/user/cros-garcon.service.d/cros-garcon-override.conf
+
+#autorun
+sudo echo '/usr/bin/fcitx-autostart' > ~/.sommelierrc
+
+sudo reboot
+
