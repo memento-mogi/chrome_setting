52e84510f088d1212679fb035f1ab0b54e704fc6
tangent-theta <86485306+tangent-theta@users.noreply.github.com>
2022-03-03 00:26:45 +0900

    Update chrome_setup.sh

--- chrome_setup.sh
+++ chrome_setup.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-# apt update&upgrade
 sudo apt update -y
 sudo apt upgrade -y
 
-#install google-chrome
+# Install google-chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
-#install fonts
+# Install fonts
 sudo apt install task-japanese locales-all fonts-ipafont -y
 sudo localectl set-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
 source /etc/default/locale
 
-#install mozc
+# Install mozc
 sudo apt install fcitx-mozc -y
 
-#set environment variable
+# Set environment variable
 sudo chmod a+w /etc/systemd/user/cros-garcon.service.d/cros-garcon-override.conf
 sudo echo 'Environment="GTK_IM_MODULE=fcitx"' >> /etc/systemd/user/cros-garcon.service.d/cros-garcon-override.conf
 sudo echo 'Environment="QT_IM_MODULE=fcitx"' >> /etc/systemd/user/cros-garcon.service.d/cros-garcon-override.conf
 sudo echo 'Environment="XMODIFIERS=@im=fcitx"' >> /etc/systemd/user/cros-garcon.service.d/cros-garcon-override.conf
 
-#autorun
+# Automation
 sudo echo '/usr/bin/fcitx-autostart' >> ~/.sommelierrc
 
 sudo reboot
